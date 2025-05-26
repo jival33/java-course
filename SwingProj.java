6a3965f7e2eb9661c5f739b6f7891969f558e77c
jival33 <138695532+jival33@users.noreply.github.com>
2025-05-26 20:40:24 +0530

    Add files via upload

---
+++ SwingProj.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.io.*;
+
+public class SwingProj {
+   public static void main(String[] args) {
+        System.out.print("hello world");
+        JFrame frame= new JFrame();
+
+        frame.setSize(500, 400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        
+   } 
+}
---
+++ datatype.java
@@ -0,0 +1,14 @@
+public class datatype {
+    public static void main(String args[]){
+        double a=2.8;
+        int b=65;
+        char c='t';
+        boolean d=true;
+        String g="jival";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(g); 
+    }
+}
