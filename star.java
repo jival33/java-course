0fae3829b8ca87f1d9c89bda1d07068dd4579eab
jival33 <138695532+jival33@users.noreply.github.com>
2025-06-11 11:22:39 +0530

    Add files via upload

---
+++ star.java
@@ -0,0 +1,13 @@
+public class star {
+    
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int n = 0; n <i+1 ; n++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
