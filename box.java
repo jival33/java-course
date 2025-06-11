dc86c84ebb4c86573bb652cb8636e4bd7950b939
jival33 <138695532+jival33@users.noreply.github.com>
2025-06-11 11:55:30 +0530

    Add files via upload

---
+++ box.java
@@ -0,0 +1,21 @@
+public class box {
+    public static void main(String[] args) {
+        
+   
+        for (int i = 0; i <= 4; i++) {
+            for (int n = 0; n <=4 ; n++) {
+                if(i>=2&&n>=2&&i<=4&&n<=4){
+                    System.out.print("");
+                }
+                else{
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            
+        }
+
+    }
+
+    }
+
